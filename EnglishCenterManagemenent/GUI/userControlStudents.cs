03a15db196293f2db9d38ad40d0eb6b20d3acf47
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-12 10:17:19 +0700

    Add some rows to "kryptonDataGridView1" to test

--- EnglishCenterManagemenent/GUI/userControlStudents.cs
+++ EnglishCenterManagemenent/GUI/userControlStudents.cs
@@ -15,6 +15,9 @@ namespace EnglishCenterManagemenent.GUI
         public userControlStudents()
         {
             InitializeComponent();
+            kryptonDataGridView1.Rows.Add(new object[] { "name1", "addr1","dob1","email1", "averagegrade1" });
+            kryptonDataGridView1.Rows.Add(new object[] { "name2", "addr2", "dob2", "email2", "averagegrade2" });
+            kryptonDataGridView1.Rows.Add(new object[] { "name3", "addr3", "dob3", "email3", "averagegrade" });
         }
     }
-}
+    }

021e26a75c33772a877a18efc1d3a04f9d50adeb
Thien <thiennguyen3132002@gmail.com>
2022-06-01 21:09:05 +0700

    DAO and DTO for Student

---
+++ EnglishCenterManagemenent/DAO/StudentDAO.cs
@@ -0,0 +1,42 @@
+﻿using EnglishCenterManagemenent.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishCenterManagemenent.DAO
+{
+    public class StudentDAO
+    {
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM dbo.STUDENT");
+            foreach (DataRow row in data.Rows)
+                students.Add(new Student(row));
+
+            return students;
+        }
+
+        public static void AddStudent(Student newStudent)
+        {
+            DataProvider.Instance.ExecuteNonQuery(
+                    "INSERT INTO STUDENT(ClassID, LastName, FirstName, " +
+                    "Address, DateOfBirth, Phone, AverageGrade) " +
+                    "VALUES ( @ClassID, @LastName, @FirstName, " +
+                    "@Address, @DateOfBirth, @Phone, @AverageGrade)",
+
+                    new object[] {
+                    newStudent.ClassID,
+                    newStudent.FirstName,
+                    newStudent.LastName,
+                    newStudent.Address,
+                    newStudent.DateOfBirth,
+                    newStudent.Phone,
+                    newStudent.AverageGrade });
+        }
+    }
+}
